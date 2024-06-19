785375fbab8005acf6e285b6b4f1802455f6a5a5
larretav <larreta_vzla@hotmail.com>
2024-06-18 17:04:01 -0700

    Ajustes en logger.midleware, se agregó un try/catch, se modificó la fecha usando dayjs y la url completa del endpoint

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -6,25 +6,29 @@ import { JwtUser } from 'src/auth/interfaces/jwt-user.interface';
 
 import * as fs from 'fs';
 import * as path from 'path';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization.split(' ')?.[1] ?? ''
+      const userData = jwtDecode<JwtUser>(token);
 
-    const token = req.headers.authorization.split(' ')?.[1] ?? ''
-    const userData = jwtDecode<JwtUser>(token);
+      const date = dayjs().format('YYYY-MM-DD HH:mm:ss')
 
-    const log = `${req.method} | ${req.baseUrl} | ${userData.user} | ${userData.role} | ${new Date().toISOString()}\n`;
-    const logDir = path.join(__dirname, '..', '..', '..', 'logs');
-    const logFilePath = path.join(logDir, 'user.log');
+      const log = `${req.method} | ${req.baseUrl}${req.url} | ${userData.user} | ${userData.role} | ${date}\n`;
+      const logDir = path.join(__dirname, '..', '..', '..', 'logs');
+      const logFilePath = path.join(logDir, 'user.log');
 
-    if (!fs.existsSync(logDir)) {
-      fs.mkdirSync(logDir);
+      if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir);
+      }
+
+      fs.appendFileSync(logFilePath, log);
+    } catch (error) {
+      console.log(`Token no válido: ${req.baseUrl}${req.url}`)
     }
-
-    // console.log('middleware', __dirname)
-    // console.log('middleware', logDir)
-    fs.appendFileSync(logFilePath, log);
 
     next();
   }
